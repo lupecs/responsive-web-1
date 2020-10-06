2be358e86c4f69d2b3bd60cc5f08e50fb6edee3a
Guadalupe Sahagun <lupecsahagun23@yahoo.com>
2020-10-06 02:22:05 -0700

    start files

---
+++ src/components/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+        <>
+            <div className={lightBg ? 'home__hero-section'}>
+
+            </div>
+        </>
+    )
+}
+
+export default HeroSection
